3f665021dce9c7060f1cffb8f041a9b38e0cdca2
SirSpectacular <sszymek.5@gmail.com>
2018-01-23 17:19:10 +0100

    EventSystem cleanup

--- src/EventSystem.hs
+++ src/EventSystem.hs
@@ -5,22 +5,26 @@ import GameState
 
 
 handleKeys :: Event -> GameState -> GameState
-
-handleKeys (EventKey (Char 'w') Graphics.Gloss.Interface.IO.Game.Down _ _) state =
- if 1 == ((abs $ 0 - (orientationToInt $ orientation state)) `mod` 2) && changeOfOrientation state == False
- then state { orientation = GameState.Up, changeOfOrientation = True }
- else state
-handleKeys (EventKey (Char 's') Graphics.Gloss.Interface.IO.Game.Down _ _) state =
- if 1 == ((abs $ 2 - (orientationToInt $ orientation state)) `mod` 2) && changeOfOrientation state == False
- then state { orientation = GameState.Down, changeOfOrientation = True }
- else state
-handleKeys (EventKey (Char 'a') Graphics.Gloss.Interface.IO.Game.Down _ _) state =
- if 1 == ((abs $ 3 - (orientationToInt $ orientation state)) `mod` 2) && changeOfOrientation state == False
- then state { orientation = GameState.Left, changeOfOrientation = True }
- else state
-handleKeys (EventKey (Char 'd') Graphics.Gloss.Interface.IO.Game.Down _ _) state =
- if 1 == ((abs $ 1 - (orientationToInt $ orientation state)) `mod` 2) && changeOfOrientation state == False
- then state { orientation = GameState.Right, changeOfOrientation = True }
- else state
-handleKeys (EventKey (Char 'r') Graphics.Gloss.Interface.IO.Game.Down _ _) state = initialGameState
+handleKeys (EventKey key Graphics.Gloss.Interface.IO.Game.Down _ _) state
+ | key == (Char 'w') =  
+  if canChangeTo GameState.Up state && changeOfOrientation state == False
+  then state { orientation = GameState.Up, changeOfOrientation = True }
+  else state
+ | key == (Char 's') =
+  if canChangeTo GameState.Down state && changeOfOrientation state == False
+  then state { orientation = GameState.Down, changeOfOrientation = True }
+  else state
+ | key == (Char 'a') =
+  if canChangeTo GameState.Left state && changeOfOrientation state == False
+  then state { orientation = GameState.Left, changeOfOrientation = True }
+  else state  
+ | key == (Char 'd') =
+  if canChangeTo GameState.Right state && changeOfOrientation state == False
+  then state { orientation = GameState.Right, changeOfOrientation = True }
+  else state
+ | key == (Char 'r') = initialGameState
+ 
 handleKeys _ state = state
+
+canChangeTo :: Orientation -> GameState -> Bool
+canChangeTo x state = ((abs $ (orientationToInt x) - (orientationToInt $ orientation state)) `mod` 2) == 1
